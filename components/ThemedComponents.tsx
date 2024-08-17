2f0dd4c84afab8bb5bc50dff1886cfe2135fcbda
andrea-leone-mcs <leoneandrea7300@gmail.com>
2024-08-17 15:39:35 +0200

    feat: add progress bar fix: signatures

--- components/ThemedComponents.tsx
+++ components/ThemedComponents.tsx
@@ -1,5 +1,6 @@
-import { Text, TextProps, View, ViewProps, useColorScheme } from "react-native";
+import { DimensionValue, StyleSheet, Text, TextProps, View, ViewProps, useColorScheme } from "react-native";
 import Colors from "../styles/Colors";
+import { useEffect } from "react";
 
 interface ThemedTextProps extends TextProps {
   children: React.ReactNode;
@@ -34,9 +35,9 @@ const ThemedView: React.FC<ThemedViewProps> = ({ children, style, ...props }) =>
   );
 };
 
-const HSpaceView = ({ size }) => <View style={{ width: size }} />;
-const VSpaceView = ({ size }) => <View style={{ height: size }} />;
-const HLine = ({ width, topMargin = 0, bottomMargin = 0 }) => <View
+const HSpaceView: React.FC<{size: number}> = ({ size }) => <View style={{ width: size }} />;
+const VSpaceView: React.FC<{size: number}> = ({ size }) => <View style={{ height: size }} />;
+const HLine: React.FC<{width: DimensionValue, topMargin: number, bottomMargin: number}> = ({ width, topMargin = 0, bottomMargin = 0 }) => <View
   style={{
     height: 1,
     width: width,
@@ -47,4 +48,40 @@ const HLine = ({ width, topMargin = 0, bottomMargin = 0 }) => <View
   }}
 />
 
-export { ThemedText, ThemedView, HSpaceView, VSpaceView, HLine };
+const CustomProgressBar: React.FC<{ label: string, progress: number, max: number }> = ({ label, progress, max }) => {
+  return (
+    <View style={{
+      justifyContent: 'center',
+      width: '100%',
+      height: 26,
+      backgroundColor: Colors.black,
+      borderRadius: 50,
+      overflow: 'hidden',
+      borderWidth: 1,
+      borderColor: Colors.light,
+    }}>
+      <View style={{
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        zIndex: 1,
+      }}>
+        <Text style={{
+          fontWeight: 'bold',
+          fontSize: 16,
+          color: Colors.white,
+        }}>
+          {label}      {progress} / {max}
+        </Text>
+      </View>
+      <View style={{
+        width: `${progress * 100 / max}%`,
+        height: 20,
+        backgroundColor: Colors.lessDark,
+      }} />
+    </View>
+  );
+}
+
+export { ThemedText, ThemedView, HSpaceView, VSpaceView, HLine, CustomProgressBar };
