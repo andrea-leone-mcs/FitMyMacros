21901fbfc0587e878998c45d72390af34532ea85
andrewl73 <leoneandrea7300@gmail.com>
2024-10-06 20:16:22 +0200

    feat: add long press to buttons

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,49 +1,87 @@
 import Ionicons from '@react-native-vector-icons/ionicons';
 import Colors from '../styles/Colors';
 import { StyleSheet } from 'react-native';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 // Button to add food to a meal
-function AddButton(props: { callback, style?}) {
+function PlusButton(props: { onPress, onLongPress?, style?}) {
   const [pressing, setPressing] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const startLongPress = () => {
+    props.onLongPress();
+    intervalRef.current = setInterval(() => {
+      props.onLongPress();
+    }, 200);
+  };
+
+  const stopLongPress = () => {
+    if (intervalRef.current)
+      clearInterval(intervalRef.current);
+  };
+
   return (
-    <Ionicons name="add-outline" size={24} color={Colors.light} onPress={props.callback}
+    <Ionicons name="add-outline" size={24} color={Colors.light}
       style={[
         styles.button,
         { backgroundColor: pressing ? Colors.darker : Colors.dark },
         props.style
       ]}
+      onPress={props.onPress}
+      onLongPress={() => {
+        startLongPress();
+      }}
       onPressIn={() => {
         setPressing(true);
       }}
       onPressOut={() => {
         setPressing(false);
+        stopLongPress();
       }}
     />
   );
 }
 
 // Button to remove food from a meal
-function RemoveButton(props: { callback, style?}) {
+function MinusButton(props: { onPress, onLongPress?, style?}) {
   const [pressing, setPressing] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const startLongPress = () => {
+    props.onLongPress();
+    intervalRef.current = setInterval(() => {
+      props.onLongPress();
+    }, 200);
+  };
+
+  const stopLongPress = () => {
+    if (intervalRef.current)
+      clearInterval(intervalRef.current);
+  };
+
   return (
-    <Ionicons name="remove-outline" size={24} color={Colors.light} onPress={props.callback}
+    <Ionicons name="remove-outline" size={24} color={Colors.light}
       style={[
         styles.button,
         { backgroundColor: pressing ? Colors.darker : Colors.dark },
         props.style
       ]}
+      onPress={props.onPress}
+      onLongPress={() => {
+        startLongPress();
+      }}
       onPressIn={() => {
         setPressing(true);
       }}
       onPressOut={() => {
         setPressing(false);
+        stopLongPress();
       }}
     />
   );
 }
 
-export { RemoveButton, AddButton };
+export { MinusButton, PlusButton };
 
 const styles = StyleSheet.create({
   button: {
